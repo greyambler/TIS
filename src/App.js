b1febce0b01cfce72e65a864227135aba5f5df04
greyambler <greyambler@rambler.ru>
2019-04-03 17:00:35 +0300

    N5
    Добавлено Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
-import MainWindow from './MainWindow.jsx'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
+//import MainWindow from './MainWindow.jsx'
 import Header from './core/Header';
+
+class About extends React.Component{
+   render(){
+       return <h2>О сайте</h2>;
+   }
+}
+class NotFound extends React.Component{
+   render(){
+       return <h2>Ресурс не найден</h2>;
+   }
+}
+ 
+class Main extends React.Component{
+   render(){
+       return <h2>Главная</h2>;
+   }
+}
 
 class App extends Component {
    render() {
@@ -8,7 +28,27 @@ class App extends Component {
          <div>
             <Header/>
             <div className="content">
-               <MainWindow/>
+            <Router>
+      <div>
+        <nav>
+          <ul className='hr'>
+            <li>
+              <Link to="/">Main</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">NotFound</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Main} />
+        <Route path="/about/" component={About} />
+        <Route path="/users/" component={NotFound} />
+      </div>
+    </Router>
             </div>
          </div>
       );
