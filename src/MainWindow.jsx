4c40d37bcd16141fa3cc576dd7a514ac50b99157
greyambler <greyambler@rambler.ru>
2019-04-03 16:49:55 +0300

    N4
    В проект добавлен заголовок и  подвал.

---
+++ src/MainWindow.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const _Debuge = false;
+
+class MainWindow extends React.Component {
+   constructor(props) {
+      super(props);
+   }
+
+   render() {
+      return (
+         <div>
+            <center><h1>Рабочая область!</h1></center>
+         </div>
+      );
+   }
+}
+
+export default MainWindow;
---
+++ src/core/Header.js
@@ -0,0 +1,27 @@
+import React, { Component, PropTypes } from 'react';
+
+class Header extends Component {
+   constructor(props, context) {
+      super(props, context);
+   }
+   render() {
+      return (
+         <div className="header">
+            <div className="header_Inner">
+               <img className="header_Img"
+                  src={'./images/favicon.ico'}
+                  alt="React"
+                  width="30"
+                  height="30"
+               />
+               <div className="header_Text">
+                  <h3> Временная ИС по системным инцидентам</h3>
+               </div>
+
+            </div>
+         </div>
+      );
+   }
+}
+
+export default Header;
